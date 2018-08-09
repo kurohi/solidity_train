96814d7d1390a09499e1a169259a49ed9e3bca0f
Igor Coelho <igor@sc0ville.com>
2018-08-09 19:09:40 +0900

    updating the campaign contract

--- campaign_contract/campaign.sol
+++ campaign_contract/campaign.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
     address public manager;
     address[] private approvers;
     uint public min_contribution;
     uint public min_2b_approver;
+    Request[] public requests;
     
     constructor(uint min_contrib, uint min_approver) public {
         manager = msg.sender;
@@ -22,5 +29,20 @@ contract Campaign {
     function getApprovers() public view returns(address[]){
         require(msg.sender == manager);
         return approvers;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request new_request = Request({
+            description: description, 
+            value: value, 
+            recipient: recipient, 
+            complete: false});
+            
+        requests.push(new_request);
+    }
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
     }
 }
